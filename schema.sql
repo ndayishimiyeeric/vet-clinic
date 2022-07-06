0a52e4f094f31696b31c9b6d517e38d0a0496998
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-07-06 17:54:17 +0200

    Add new table and foreign keys

--- schema.sql
+++ schema.sql
@@ -10,3 +10,33 @@ weight_kg DECIMAL,
 PRIMARY KEY(id));
 
 ALTER TABLE animals ADD species varchar(200);
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+full_name varchar(250),
+age INT,
+PRIMARY KEY(id));
+
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+name varchar(200),
+PRIMARY KEY(id));
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT species_key
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT owners_key
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
