0010f26b121e1c51e69e4eb03e0b9363c28d3b28
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-07-06 17:53:41 +0200

    Add data

--- data.sql
+++ data.sql
@@ -14,3 +14,42 @@ VALUES ('Charmander', '2020-02-08', false, -11, 0),
 ('Boarmon', '2005-06-07', true, 20.4, 7),
 ('Blossom', '1998-10-13', true, 17, 3),
 ('Ditto', '2022-05-14', true, 22, 4);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species(name)
+VALUES ('Pokemon'),
+('Digimon');
+
+UPDATE animals
+SET species_id = 1;
+
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = 3
+WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Charamander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals
+SET owner_id = 5
+WHERE name = 'Boarmon' OR name = 'Angemon';
